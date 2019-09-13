811ac7fc666e8cb2ea350395f8a062dd55fabc26
Ivan Kozlov <ikozlov@student.42.us.org>
2019-09-12 18:41:55 -0700

    Fix issue: reg expression match is too big.
    Add ? to cancel greedy behavior

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -1,20 +1,27 @@
 #!/usr/bin/env php
 <?php
-  function replace($matches){
+  function replace($matches) {
     return $matches[1].strtoupper($matches[2]).$matches[3];
   }
+
+  function replace2($matches) {
+    $reg = "/(>)(.*?)(<)/i";
+    $matches[2] = preg_replace_callback($reg, 'replace', $matches[2]);
+    return $matches[1].$matches[2].$matches[3];
+  }
+
   if ($argc > 1) {
     $reg1 = "/(<a.*?title=\")(.*)(\">)/i";
-    $reg2 = "/(<a.*?>)(.*)(<)/i";
+    $reg2 = "/(<a.*?)(.*)(\/a>)/i";
     $file = "";
     $fd = fopen($argv[1], "r");
     while (!feof($fd))
     {
       $line = fgets($fd);
-      $line = preg_replace_callback($reg1, replace, $line);
-      $line = preg_replace_callback($reg2, replace, $line);
+      $line = preg_replace_callback($reg1, 'replace', $line);
+      $line = preg_replace_callback($reg2, 'replace2', $line);
       $file .= $line;
     }
-    print("{$file}\n");
+    print("{$file}");
   }
 ?>
